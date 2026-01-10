e5e417bd8565a4acece900130a5ac06ace2a58b5
CyberTempest1w <samet102a@gmail.com>
2026-01-10 20:48:51 +0300

    add user validate

---
+++ validations/userValidate.js
@@ -0,0 +1,10 @@
+const joi = require("joi");
+
+module.exports.userValidation = joi.object({
+  username: joi.string().min(3).required(),
+  password: joi.string().min(3).required(),
+  email: joi.string().email().required(),
+  isAdmin: joi.boolean(),
+  comments: joi.array(),
+  date: joi.date(),
+});
