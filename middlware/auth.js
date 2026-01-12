b9d2823fafeb692323283029ec62e2858e5e334b
CyberTempest1w <samet102a@gmail.com>
2026-01-12 16:49:22 +0300

    add auth middlware

---
+++ middlware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports.access = (req, res, next) => {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(401).json({ message: "Access denied" });
+  }
+  try {
+    const decoded = jwt.verify(token, "secretkey");
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
